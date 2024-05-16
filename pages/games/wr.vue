c5021c7febeaee11863b66909b32a76b14257f26
Federico Mantovani <federico16905@gmail.com>
2024-05-16 21:27:34 +0200

    premiers changements user guide

--- pages/games/wr.vue
+++ pages/games/wr.vue
@@ -1,7 +1,15 @@
 <script setup>
+
 </script>
+
 <template>
+    <div class="card">
+        <div class="card-nav">
+            
+        </div>
+    </div>
 </template>
 
 <style scoped>
+
 </style>
