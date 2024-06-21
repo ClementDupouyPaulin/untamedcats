d0e42f866fce3e6cc4c40f956d1badc79da33fa1
Federico Mantovani <federico16905@gmail.com>
2024-06-21 09:42:44 +0200

    front messages + callApi

--- pages/forum/index.vue
+++ pages/forum/index.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
 const route = useRoute()
 const apiCall = await useFetch('/getCategories')
+const apiCallMessages = await useFetch('/getLatestMessages')
 
 </script>
 <template>
   <div class="content">
-    {{ apiCall }}
     <div class="card">
       <div class="card-nav">
          <h3>{{ $t('forum.welcome.title') }}</h3> 
@@ -26,11 +26,11 @@ const apiCall = await useFetch('/getCategories')
           <div class="card-body">
             <p>{{ $t('forum.categories.body') }}</p>
           </div>
-          <!-- <ul>
-            <li v-for="category in categories.categories">
-s                <NuxtLink :to="'forum/' + category.id">{{ category.name }}</NuxtLink>
+          <ul>
+            <li v-for="category in apiCall.data.value.categories">
+              <NuxtLink class="link" :to="'forum/' + category.id">{{ category.name }}</NuxtLink>
             </li>
-          </ul> -->
+          </ul>
         </div>
       </div>
       <div class="col-md-6">
@@ -41,8 +41,18 @@ s                <NuxtLink :to="'forum/' + category.id">{{ category.name }}</Nux
     
           <div class="card-body">
             <p>{{ $t('forum.latest.body') }}</p>
+            <div v-for="message in apiCallMessages.data.value.messages">
+              <div class="card message">
+                <div class="card-nav">
+                  <h3>{{ message.title }}</h3>
+                </div>
+    
+                <div class="card-body">
+                  <p>{{ message.content }}</p>
+                </div>
+              </div>
+            </div>
           </div>
-          
         </div>
       </div>
     </div>
@@ -50,4 +60,12 @@ s                <NuxtLink :to="'forum/' + category.id">{{ category.name }}</Nux
 </template>
 <style scoped>
 p {text-align: center;}
+ul {text-align: center;}
+li {list-style-type: none; text-decoration: none;}
+.link {text-decoration: none; color: #212529}
+.link:hover {text-decoration: none; color: #FFFFFF}
+.message{background-color: rgba(255, 255, 255, 0.8); margin-top: 30px;}
+.message:hover{background-color: grey; transition-property: background-color; transition-duration: 0.3s; transition-delay: 0.05s; cursor: pointer}
+.message:hover h3{color: white;background: none; transition-property: color; transition-duration: 0.3s; transition-delay: 0.05s;}
+.message:hover {color: lightgrey; transition-property: color; transition-duration: 0.3s; transition-delay: 0.0005s;}
 </style>
--- pages/message/[id].vue
+++ pages/message/[id].vue
@@ -1,23 +1,29 @@
 <script setup>
   const route = useRoute()
-  const apiCall = await useFetch('/getMessagesById/' + route.params.id)
+  const messages = ref()
+  useFetch('/getMessagesById/' + route.params.id).then((response) => {
+    
+    messages.value = response.data.value.messages
+  })
 
 </script>
 
 <template>
   <div class="content">
-    <div class="card">
-      <div class="card-nav">
-         <h3>{{ $t('forum.welcome.title') }}</h3> 
-      </div>
+    <div v-for="message in messages">
+      <div class="card">
+        <div class="card-nav">
+          <h3>{{ message.title }}</h3> 
+        </div>
 
-      <div class="card-body">
-        <p>{{ $t('forum.welcome.body') }}</p>
+        <div class="card-body">
+          <p>{{ message.content }}</p>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <style scoped>
-
-</style>
+p {text-align: center; font-size: 1.5rem;}
+</style>  
